cd4e8e4bd092c1d57ec21acfa48c72318a70ca40
fox102447@gmail.com <fox102447@gmail.com>
2025-03-30 11:50:13 +0300

    the first working option

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,27 @@ int main(){
 	int* mem = new int[MAX_SIZE]{};
 	int* ptr = mem;
 
-	std::string s = "+++>++[<->+]";
-	for (size_t i = 0; i < s.size(); ++i) {
-		if(s[i] == '+') ++(*ptr);
-		if(s[i] == '-') --(*ptr);
-		if(s[i] == '>') ++ptr;
-		if(s[i] == '<') --ptr;
+	std::string s = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.";
+	for (size_t i = 0; i < s.size(); i++) {
+		if (std::isspace(s[i])) continue;
+		if (s[i] == '+') {
+			++(*ptr);
+			if (*ptr > 255) *ptr = 0;
+		}
+		if (s[i] == '-') { 
+			--(*ptr); 
+			if (*ptr < 0) *ptr = 255;
+		}
+		if (s[i] == '.') std::cout << (char)(*ptr);
+		if (s[i] == '>') {
+			++ptr;
+			if (ptr >= mem + MAX_SIZE) { 
+				ptr = mem + MAX_SIZE - 1;
+			}
+		}
+		if (s[i] == '<') {
+			if (ptr > mem) --ptr;
+		}
 		if (s[i] == '[') {
 			if (*ptr == 0) {
 				int brc = 1;
@@ -23,16 +38,13 @@ int main(){
 			}
 		}
 		else if (s[i] == ']') {
-			if (*ptr != 0) {
+			if(*ptr != 0) {
 				int brc = 1;
 				while (brc > 0 && i > 0) {
 					char curr_char = s[--i];
 					if (curr_char == '[') brc--;
 					if (curr_char == ']') brc++;
 				}
-				--i;
-			} else {
-				++i;
 			}
 		}
 	}
